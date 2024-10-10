28282794d3bb6304da798d34f80bb6c4694a0119
PetterFogel <petterfogel96@gmail.com>
2024-10-10 20:15:22 +0200

    Implement logic to register user by post

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+
+const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<Response | any> => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExist = await User.findOne({ email });
+
+    if (userExist)
+      return res.status(400).json({ message: "User Already Exists" });
+
+    const user = await User.create({ name, email, password });
+
+    if (user) {
+      return res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    }
+
+    res.status(400).json({ message: "Invalid User Data" });
+  } catch (error) {
+    console.log("Register User Error: ", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export { registerUser };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import connectDatabase from "./config/db";
+import userRoutes from "./routes/userRoutes";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 connectDatabase();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Hello World!");
